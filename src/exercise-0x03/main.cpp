a75651cfb5318f43ee6491868a777b635f29dec0
Yolk <Joachim.schousboe@gmail.com>
2024-09-27 11:27:39 +0000

    Exercise 4

--- src/exercise-0x03/main.cpp
+++ src/exercise-0x03/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cmath>
 
-int main() {
-  std::cout << "Exercise 0x03" << std::endl;
+/*Exercise 0x03
+Extract one (or more) function(s) from the code below, that makes the program more comprehensible.*/
+
+int main(int argc, char** argv) {
+  
+  double v, r;
+
+  std::cout << "This program calculates the power dissipated in a simple resistor circuit" << std::endl;
+  std::cout << "Input the power source voltage [V]: " << std::endl;
+  std::cin >> v;
+  std::cout << "Input the resistance R [Ohm]: " << std::endl;
+  std::cin >> r;
+
+  double power = pow(v, 2) / r;
+  
+  std::cout << "The power dissipated in R is " << power << "watts [W]" << std::endl;
+  
   return 0;
 }
